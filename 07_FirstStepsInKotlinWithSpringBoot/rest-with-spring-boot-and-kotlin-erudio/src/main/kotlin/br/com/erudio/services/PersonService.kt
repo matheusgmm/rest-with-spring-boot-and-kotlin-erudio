df303a852c7e79c609274f11332af7239e4a9278
Matheus Muccio <matheus.muccio@rodojacto.com.br>
2024-01-30 10:38:33 -0300

    adding section 08, creating mysql database

---
+++ 07_FirstStepsInKotlinWithSpringBoot/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/services/PersonService.kt
@@ -0,0 +1,58 @@
+package br.com.erudio.services
+
+import br.com.erudio.model.Person
+import org.springframework.stereotype.Service
+import java.util.logging.Logger
+import java.util.concurrent.atomic.AtomicLong
+
+
+@Service
+class PersonService {
+
+    private val counter: AtomicLong = AtomicLong();
+
+    private val logger = Logger.getLogger(PersonService::class.java.name);
+
+    fun findAll(): List<Person> {
+        logger.info("Finding all people!");
+        val persons: MutableList<Person> = ArrayList();
+
+        for (i in 0..7) {
+            val person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    fun findById(id: Long): Person {
+        logger.info("Finding one person!");
+        val person = Person();
+
+        person.id = counter.incrementAndGet();
+        person.firstName = "Matheus";
+        person.lastName = "Muccio";
+        person.address = "Tupã - São Paulo - Brasil";
+        person.gender = "Male";
+        return person;
+    }
+
+//    fun create(person: Person): Person {
+//        return person;
+//    }
+
+    fun create(person: Person) = person;
+
+    fun update(person: Person) = person;
+
+    fun delete(id: Long) { }
+
+    private fun mockPerson(i: Int): Person {
+        val person = Person();
+        person.id = counter.incrementAndGet();
+        person.firstName = "Person Name $i";
+        person.lastName = "Last Name $i";
+        person.address = "Some Address in Brasil";
+        person.gender = "Male";
+        return person;
+    }
+}
