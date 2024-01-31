de61d43f77bbd5c59e7d49f602f06e4c9f2e4531
Matheus Muccio <matheus.muccio@rodojacto.com.br>
2024-01-31 08:01:01 -0300

    finish section 09 VO (Value Object)

--- 09_FirstStepsInKotlinWithSpringBoot/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/controller/PersonController.kt
+++ 09_FirstStepsInKotlinWithSpringBoot/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/controller/PersonController.kt
@@ -1,18 +1,11 @@
 package br.com.erudio.controller
 
-import br.com.erudio.model.Person
+import br.com.erudio.data.vo.v1.PersonVO
 import br.com.erudio.services.PersonService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/person")
@@ -22,24 +15,24 @@ class PersonController {
     private lateinit var service: PersonService;
 
     @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun findALl(): List<Person> {
+    fun findALl(): List<PersonVO> {
         return service.findAll();
     }
 
     @GetMapping(value=["/{id}"], produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun findById(@PathVariable(value="id") id: Long ): Person {
+    fun findById(@PathVariable(value="id") id: Long ): PersonVO {
         return service.findById(id);
     }
 
     @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE],
                  produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun create(@RequestBody person: Person): Person {
+    fun create(@RequestBody person: PersonVO): PersonVO {
         return service.create(person);
     }
 
     @PutMapping(consumes = [MediaType.APPLICATION_JSON_VALUE],
                 produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun update(@RequestBody person: Person): Person {
+    fun update(@RequestBody person: PersonVO): PersonVO {
         return service.update(person);
     }
 
