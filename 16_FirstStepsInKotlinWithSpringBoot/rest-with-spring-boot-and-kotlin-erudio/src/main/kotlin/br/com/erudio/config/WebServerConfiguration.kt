57fe8df68bd494053ac82bf61da938bb9d345e65
Matheus Muccio <matheus.muccio@rodojacto.com.br>
2024-02-05 10:00:28 -0300

    section 16: CORS

---
+++ 16_FirstStepsInKotlinWithSpringBoot/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/config/WebServerConfiguration.kt
@@ -0,0 +1,20 @@
+package br.com.erudio.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+//@Configuration
+class WebServerConfiguration {
+
+
+
+    fun addCorsConfig(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                super.addCorsMappings(registry)
+            }
+        }
+    }
+}
---
+++ 16_FirstStepsInKotlinWithSpringBoot/rest-with-spring-boot-and-kotlin-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO `person` VALUES
+(1,'Tupã - São Paulo - Brasil','Matheus','Male','Muccio'),
+(3,'teste','teste','teste','teste'),
+(4,'teste','teste','teste','teste');
+
